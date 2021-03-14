10cf3e2174b04c219f4956e76e010142a28e777d
Nataliee <nataliekc71@gmail.com>
2021-03-14 14:35:53 -0700

    Bind About button's onClick event and its methods logic
    Toggle accessibility of drawer(side bar)

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,6 @@
   <v-app>
     <v-app-bar app color="#FFFFFF">
       <div>
-        <!-- <v-img
-          alt="Polygon Logo"
-          class="shrink mr-2"
-          contain
-          src="https://hellopolygon.com/img/logo.abde79e7.svg"
-          transition="scale-transition"
-          width="100"
-        /> -->
         <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
@@ -21,38 +13,102 @@
       </div>
 
       <v-spacer></v-spacer>
-      <v-btn target="_blank" text @click="showMenu">
-        <span class="mr-2">Services</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn target="_blank" text>
+        <button
+          href="#"
+          data-drawer-trigger
+          aria-controls="drawer-name"
+          aria-expanded="false"
+          @click="openClickHandler"
+        >
+          About Us
+          <!--          <v-icon>mdi-open-in-new</v-icon>-->
+        </button>
+        <child-card :settings="settings"></child-card>
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <HelloWorld />
-      <!-- <Menu /> -->
+      <About />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
-//import Menu from "./components/Menu";
+// import Reviews from "./components/Reviews";
+import About from "./components/About";
 export default {
-  name: "App",
+  name: "Appnew",
 
   components: {
     HelloWorld,
-    //Menu,
+    About,
   },
-
   data: () => ({
-    showMenu: false,
+    settings: {
+      speedOpen: 50,
+      activeClass: "is-active",
+      visibleClass: "is-visible",
+      selectorTarget: "[data-drawer-target]",
+      selectorTrigger: "[data-drawer-trigger]",
+    },
   }),
   methods: {
-    showMenu() {
-      //console.log(88);
-      this.showMenu = !this.showMenu;
+    // Toggle accessibility
+    toggleAccessibility: function (event) {
+      if (event.getAttribute("aria-expanded") === "true") {
+        event.setAttribute("aria-expanded", false);
+      } else {
+        event.setAttribute("aria-expanded", true);
+      }
+    },
+    // Open Drawer
+    openDrawer(trigger) {
+      // Find target
+      var target = document.getElementById(
+        trigger.getAttribute("aria-controls")
+      );
+      // Make it active
+      target.classList.add(this.settings.activeClass);
+      // Make body overflow hidden so it's not scrollable
+      document.documentElement.style.overflow = "hidden";
+      // Toggle accessibility
+      this.toggleAccessibility(trigger);
+
+      // Make it visible
+      setTimeout(function () {
+        target.classList.add("is-visible");
+      }, this.settings.speedOpen);
+    },
+
+    // Open Handler
+    openClickHandler(event) {
+      var open = event.target;
+      if (open) {
+        this.openDrawer(open);
+
+        // Prevent default link behavior
+        event.preventDefault();
+      }
+    },
+
+    // Keydown Handler, handle Escape button
+    keydownHandler(event) {
+      if (event.key === "Escape" || event.keyCode === 27) {
+        // Find all possible drawers
+        var drawers = document.querySelectorAll(this.settings.selectorTarget),
+          i;
+        // Find active drawers and close them when escape is clicked
+        for (i = 0; i < drawers.length; ++i) {
+          if (drawers[i].classList.contains(this.settings.activeClass)) {
+            this.closeDrawer(drawers[i]);
+          }
+        }
+      }
     },
   },
+  // window.addEventListener('clickHandler')
 };
 </script>
